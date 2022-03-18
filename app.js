26688eedc37679d6a0ba27fb2b0702e1b59fdcf6
Yann CLS <cleonisyann@icloud.com>
2022-03-18 10:38:24 +0100

    .env

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv').config()
-console.log(process.env)
 
 const app = express();
 
