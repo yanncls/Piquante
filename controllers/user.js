f10d2d7c4caf889617df1057b1532a19741edce8
Yann CLS <cleonisyann@icloud.com>
2022-04-28 10:30:56 +0200

    last corrections

--- controllers/user.js
+++ controllers/user.js
@@ -1,63 +1,70 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const validator = require('validator');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-const user = require('../models/user');
+const user = require("../models/user");
 
 // Logique métier user
 // Création de nouveaux users
 exports.signup = (req, res, next) => {
-	// validator pour vérifier le format mail attendu
-	if (!validator.isEmail(req.body.email))
-		return res.status(403).json({ message: 'Le format du mail est incorrect.' });
-	// validator pour vérifier le format psswrd attendu
-	if (validator.isStrongPassword(req.body.password)) {
-		// bcrypt hash de password
-		bcrypt
-			.hash(req.body.password, 10)
-			.then((hash) => {
-				const myUser = new user({
-					email: req.body.email,
-					password: hash,
-				});
+  // validator pour vérifier le format mail attendu
+  if (!validator.isEmail(req.body.email))
+    return res
+      .status(403)
+      .json({ message: "Le format du mail est incorrect." });
+  // validator pour vérifier le format psswrd attendu
+  if (validator.isStrongPassword(req.body.password)) {
+    // bcrypt hash de password
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const myUser = new user({
+          email: req.body.email,
+          password: hash,
+        });
 
-				// sauvegarde dans la bdd
-				myUser
-					.save()
-					.then(() => res.status(201).json({ message: 'Utilisateur créé.' }))
-					.catch((error) => res.status(400).json({ error }));
-			})
-			.catch((error) => res.status(500).json({ error }));
-	} else
-		return res.status(403).json({
-			message:
-				'Votre mot de passe doit contenir 8 caractères minimum. Une lettre majuscule. Une lettre minuscule. Un chiffre.',
-		});
+        // sauvegarde dans la bdd
+        myUser
+          .save()
+          .then(() => res.status(201).json({ message: "Utilisateur créé." }))
+          .catch((error) => res.status(400).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  } else
+    return res.status(403).json({
+      message:
+        "Votre mot de passe doit contenir 8 caractères minimum. Une lettre majuscule. Une lettre minuscule. Un chiffre. Un caractère spécial.",
+    });
 };
 
 // login de l'user
 exports.login = (req, res, next) => {
-	// recherche du mail associé
-	user.findOne({ email: req.body.email })
-		.then((user) => {
-			if (!user) {
-				return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-			}
+  // recherche du mail associé
+  user
+    .findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
 
-			// comparer le mdp dans la bdd avec celui de la requête
-			bcrypt
-				.compare(req.body.password, user.password)
-				.then((valid) => {
-					if (!valid) {
-						return res.status(401).json({ error: 'Mot de passe incorrect !' });
-					}
-					res.status(200).json({
-						// création d'un token pour sécuriser le compte
-						userId: user._id,
-						token: jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' }),
-					});
-				})
-				.catch((error) => res.status(500).json({ error }));
-		})
-		.catch((error) => res.status(500).json({ error }));
+      // comparer le mdp dans la bdd avec celui de la requête
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            // création d'un token pour sécuriser le compte
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              process.env.ACCESS_TOKEN_SECRET,
+              { expiresIn: "24h" }
+            ),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
 };
